b3dca01ad58140ad01e11f91a05a30f9ced0f27b
ulyanapolonskaya <ulyana.polonskaya@ventionteams.com>
2025-06-05 20:30:37 +0200

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/birthday-app/',
-})
+  base: command === 'build' ? '/birthday-app/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+}))
